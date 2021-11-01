29a580aaf2e0dfb2cd064650d5fff3523cc7ebce
cnisidis <makeithappen@nisidis.com>
2021-11-01 11:37:25 +0200

    Kubleka -> Brecth (Neos Kosmos) init

---
+++ src/MACHINE.Utils/MACHINEStates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MACHINE.Utils
+{
+    public enum MACHINEStates
+    {
+        OPERATING_CUES,
+        OPERATING_SELECTION,
+        SETTINGS,
+        RELOAD,
+        DEBUG,
+        CLOSING,
+        OPENING
+    }
+}
